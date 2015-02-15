a375d4224d98c8ee3055f76b59a4b825fa459af3
benjamin saravia <elbenja@gmail.com>
2015-02-15 08:36:23 -0500

    before modifying the controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+
   devise_for :users
   # resources :links
  
@@ -7,6 +9,7 @@ resources :links do
     put "like", to: "links#upvote"
     put "dislike", to: "links#downvote"
   end
+   resources :comments
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
