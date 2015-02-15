8d8e0c505eacbebd1b992faa3fce9ff432a3978b
benjamin saravia <elbenja@gmail.com>
2015-02-15 09:07:13 -0500

    comments added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,39 +1,34 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
-
-  def index
-    @comments = Comment.all
-    respond_with(@comments)
-  end
-
-  def show
-    respond_with(@comment)
-  end
-
-  def new
-    @comment = Comment.new
-    respond_with(@comment)
-  end
-
-  def edit
-  end
-
+ 
   def create
-    @comment = Comment.new(comment_params)
-    @comment.save
-    respond_with(@comment)
+      @link = Link.find(params[:link_id])
+      @comment = @link.comments.new(comment_params)
+      @comment.user = current_user
+     
+      respond_to do |format|
+        if @comment.save
+          format.html { redirect_to @link, notice: 'Comment was successfully created.' }
+          format.json { render json: @comment, status: :created, location: @comment }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @comment.errors, status: :unprocessable_entity }
+        end
+      end
+    # @comment = Comment.new(comment_params)
+    # @comment.save
+    # respond_with(@comment)
   end
 
-  def update
-    @comment.update(comment_params)
-    respond_with(@comment)
-  end
+ 
 
   def destroy
     @comment.destroy
-    respond_with(@comment)
+    respond_to do |format|
+      format.html { redirect_to :back, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
